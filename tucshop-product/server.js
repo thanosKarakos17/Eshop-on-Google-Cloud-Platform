3cd90b22ecd22469f49b5fff8285a08b1ca7c128
Thanos R.R <karakos.th@gmail.com>
2024-10-21 01:44:20 +0300

    working

--- tucshop-product/server.js
+++ tucshop-product/server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongo = require('mongoose');
 const app = express();
+const cors = require('cors');
 
 mongo.connect(process.env.MONGO_URL);
 const db = mongo.connection;
 
 db.on('open', () => {console.log('connected to db');app.listen(5000, () => {console.log('server started')})});
 app.use(express.json());
+app.use(cors());
 const productsRouter = require('./routes/products');
 app.use('/products', productsRouter);
